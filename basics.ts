bb10ee7287b0a00efea19ff342ebf6628d5e4d2c
panwaranuj01 <anuj@kommunicate.io>
2023-07-06 22:46:35 +0530

    learn about generics how it's work under the hood

--- basics.ts
+++ basics.ts
@@ -43,3 +43,33 @@ type Person = {
 
 let arr: Person[];
 arr = [{ name: "sdf", age: 234234 }];
+
+//---------------------------------- function types --------------------------------//
+
+// explicitly setting the return type of the function
+// ts can also handle this for us
+
+// if we not define return type here this will call type inference
+const addTwo = (a: number, b: number): number => {
+  return a + b;
+};
+console.log(addTwo(12, 23));
+
+const voidReturn = (a: any, b: any) => {
+  // return a + b;
+};
+
+//---------------------------------- Generics --------------------------------//
+
+// generics always look the first argument
+// if argument is number then we can only pass number array
+function returnNewArray<A>(valueToAdd: A, array: A[]) {
+  const newArray = [valueToAdd, ...array];
+  return newArray;
+}
+
+const stringARR: string[] = ["a", "b", "c", "d", "e"];
+returnNewArray("c", stringARR);
+
+const numberARR: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+returnNewArray(3, stringARR); // got error because first argument is 
--- with-typescript.js
+++
@@ -1,5 +0,0 @@
-function add(a, b) {
-    return a + b;
-}
-var val = add(309, 3);
-console.log(val);
